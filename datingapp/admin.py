be608ec6f5652df77753181bc89eac93ec2da62e
SHAHABAS AHAMMED AK <68606288+ShahabasAK@users.noreply.github.com>
2023-05-25 20:57:12 +0530

    Delete datingapp directory

--- datingapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from.models import *
-admin.site.register(signn)
-admin.site.register(Profile)
-admin.site.register(Like)
-admin.site.register(Match)
-admin.site.register(Message)
-admin.site.register(Block)
-admin.site.register(Request_users)
